7912fdfe4059003fb072c619889465b20a51873f
Daniel Roa <lion35528@gmail.com>
2024-10-30 22:43:29 -0500

    add constants and some styles to box experience

---
+++ src/constants/index.ts
@@ -0,0 +1,37 @@
+export const EXPERIENCE = [
+    {
+        "job": "Consultor de desarrollo",
+        "companyName": "SETI S.A.S",
+        "duration": "Desde 01-03-2024 hasta presente",
+        "description": "Automatización de la carga de datos en XML y JSON utilizando Python y consumo APIs para integrar información. Desarrollando y manteniendo procedimientos almacenados, jobs, triggers y consultas complejas en SQL Server para optimizar el rendimiento de bases de datos. Implemento procesos ETL con SSIS y desarrollo APIs en C# .NET, estableciendo conexiones con bases de datos y automatizando la carga de datos.",
+        "stackImages": {
+            "python": "python image ",
+            "sql": "sql image",
+            "sqlServer": "sqlServer image",
+            "SSIS": "SSIS image",
+            ".NET": ".NET image"
+        }
+    },
+    {
+        "job": "Agente de servicio al cliente bilingüe",
+        "companyName": "Teleperformance",
+        "duration": "Desde 11-01-2024 hasta 29-02-2024",
+        "description": "Atendí peticiones y consultas en inglés para clientes de Citibank, utilizando Excel de forma intermedia para realizar cálculos de intereses y aumentos de crédito.Además, adquirí conocimientos clave sobre el sector financiero.",
+        "stackImages": {
+            "Excel": "Excel image",
+            "Ingles": "Ingles Image",
+            "ServicioAlCliente": "Servicio al cliente image",
+        }
+    },
+    {
+        "job": "Automatizador de pruebas",
+        "companyName": "TCS Tata Consultancy Services",
+        "duration": "Desde 27-04-2022 hasta 17-06-2023",
+        "description": "Atendí peticiones y consultas en inglés para clientes de Citibank, utilizando Excel de forma intermedia para realizar cálculos de intereses y aumentos de crédito.Además, adquirí conocimientos clave sobre el sector financiero.",
+        "stackImages": {
+            "Excel": "Excel image",
+            "Ingles": "Ingles Image",
+            "ServicioAlCliente": "Servicio al cliente image",
+        }
+    }
+]
